5c8b7301f3cf633485fce47a67231d882bcba256
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-07-01 12:02:36 -0300

    pagex e pagey identificam onde esta o mouse

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+// valor inicial
 let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
+
+// Events
 
 // seleciona a cor ao clicar no quadro
 document.querySelectorAll('.colorArea .color').forEach(item => {
   item.addEventListener('click', colorClickEvent);
 });
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 
+
+// functions
 function colorClickEvent(e){
   let color =e.target.getAttribute('data-color');
   console.log('cor selecionada', color);
@@ -14,5 +28,29 @@ function colorClickEvent(e){
 
   // adiciona a classe ativa
   e.target.classList.add('active');
+
+}
+
+function mouseDownEvent(){
+  canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
+
+}
+function mouseMoveEvent(e){
+  if(canDraw){
+    canDraw(e.pageX, e.pageY);
+
+  }
+  // pagex e pagey identificam onde esta o mouse
+}
+function mouseUpEvent(){
+  canDraw = false;
+  
+}
+
+function draw(x, y){
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
 
 }
